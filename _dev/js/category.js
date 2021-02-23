2ee287b25dd94bb37931294986e8388c58368bf9
Midgardsormen <mdecourtray11@gmail.com>
2021-02-23 20:54:03 +0100

    style hp + PLP

---
+++ _dev/js/category.js
@@ -0,0 +1,4 @@
+const categoryHeader = document.getElementById('js-product-list-header');
+const breadcrumbElement = document.getElementById('breadcrumbElement');
+
+breadcrumbElement.insertAdjacentElement('afterend', categoryHeader);
--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -36,6 +36,7 @@ import './customer';
 import './listing';
 import './product';
 import './cart';
+import './category';
 
 import prestashop from 'prestashop';
 import EventEmitter from 'events';
